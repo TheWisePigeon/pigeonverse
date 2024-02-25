bb1d1d9ebe5306a913506fda29a2e1f165cd0fdb
TheWisePigeon <zozozozeph@gmail.com>
2024-02-25 21:48:44 +0000

    serve static files

--- server/routes.go
+++ server/routes.go
@@ -7,5 +7,6 @@ import (
 )
 
 func RegisterRoutes(mux *http.ServeMux, fs *embed.FS) {
+	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
 	mux.Handle("GET /", handlers.RenderLandingPage(fs))
 }
