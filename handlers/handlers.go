df59bb049a9fc82c12a364f391baef1928b6915b
TheWisePigeon <zozozozeph@gmail.com>
2024-02-26 20:29:40 +0000

    working on posts page

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,7 +4,15 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
+
+type PostInfo struct {
+	Title    string `json:"title"`
+	PostedAt string `json:"posted_at"`
+	Slug     string `json:"slug"`
+}
 
 func RenderLandingPage() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -25,13 +33,35 @@ func RenderLandingPage() http.Handler {
 
 func RenderPostsPage() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		postsData := []PostInfo{}
+		entry, err := os.ReadDir(os.Getenv("CONTENT_DIR"))
+		if err != nil {
+			log.Println("Error while reading content dir:", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		for _, file := range entry {
+			slug := strings.TrimSuffix(file.Name(), ".md")
+			parts := strings.Split(slug, "-")
+			title := strings.ReplaceAll(parts[0], "_", " ")
+			postedAt := strings.ReplaceAll(parts[1], "_", " ")
+			postData := PostInfo{
+				Title:    title,
+				PostedAt: postedAt,
+				Slug:     slug,
+			}
+			postsData = append(postsData, postData)
+		}
 		templ, err := template.ParseFiles("views/base.html", "views/posts.html")
 		if err != nil {
 			log.Println("Error while parsing template", err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		err = templ.ExecuteTemplate(w, "base", nil)
+		templData := map[string]interface{}{
+			"Posts": postsData,
+		}
+		err = templ.ExecuteTemplate(w, "base", templData)
 		if err != nil {
 			log.Println("Error while rendering posts page", err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
